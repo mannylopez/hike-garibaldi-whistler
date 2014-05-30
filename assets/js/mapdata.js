cc5b7f52226a7487ee442fc7d7085791cb7459e0
manny <Manuel_Lopez@esri.com>
2014-05-30 14:52:35 -0700

    GPX data, leaflet, and omnivore data

---
+++ assets/js/mapdata.js
@@ -0,0 +1,19 @@
+var map = L.map('map').setView([50.036693, -122.970884], 13);
+
+L.esri.basemapLayer('Imagery').addTo(map);
+
+var trailFeatures = omnivore.gpx('assets/data/hike.gpx').addTo(map);
+
+// var photoFeatures = omnivore.csv('assets/data/photoLatLong.csv');
+
+// photoFeatures.on('ready', function() {
+  photoFeatures.eachLayer(function(layer) {
+    // console.log(layer.feature.properties.SourceFile);
+    layer.bindPopup('<img class="mthoodimage" src="assets/photos/' + layer.feature.properties.SourceFile + '">');
+
+  });
+}).addTo(map);
+
+//gpx.on('click', function() { alert('Clicked on a group!'); })
+
+//photoFeatures.on('click', function(e) {console.log('Clicked on a group!', e.target); })
