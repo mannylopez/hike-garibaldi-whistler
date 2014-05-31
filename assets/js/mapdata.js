56321ec6bfbaa89e12b8864861264f03df60ca11
manny <Manuel_Lopez@esri.com>
2014-05-30 17:06:38 -0700

    fixed mapdata.js

--- assets/js/mapdata.js
+++ assets/js/mapdata.js
@@ -6,14 +6,14 @@ var trailFeatures = omnivore.gpx('assets/data/hike.gpx').addTo(map);
 
 // var photoFeatures = omnivore.csv('assets/data/photoLatLong.csv');
 
-// photoFeatures.on('ready', function() {
+/* photoFeatures.on('ready', function() {
   photoFeatures.eachLayer(function(layer) {
     // console.log(layer.feature.properties.SourceFile);
     layer.bindPopup('<img class="mthoodimage" src="assets/photos/' + layer.feature.properties.SourceFile + '">');
 
   });
 }).addTo(map);
-
+*/
 //gpx.on('click', function() { alert('Clicked on a group!'); })
 
 //photoFeatures.on('click', function(e) {console.log('Clicked on a group!', e.target); })
